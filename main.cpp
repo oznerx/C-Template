371f15562252d15729ad1066a539d0f962677fb5
oznerx <A01742377@tec.mx>
2022-08-18 03:12:24 +0000

    complejidad

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
-#include "Perro.hpp"
 #include <iostream>
 
-int main()
-{
-    Perro c(4,5);
-    c.salta();
 
-    std::cout << "This is my C++ gitpod template\n";
+//Ozner Axel Leyva Mariscal 
+//A01742377
+
+
+int sumaIterativa(int n){
+    int suma = 0;
+
+    for (auto i= 1; i <= n;i++){
+        suma += i;
+    }
+
+    return suma;
 }
+
+
+//Suma recursiva con complejidad de O(n)
+int sumaRecursiva(int n){
+    if (n != 0){
+        return n + sumaRecursiva(n - 1);
+    }
+    return n;
+}
+
+//Suma directa con el metodo de Gauss para sumar números naturales 
+int sumaDirecta(int n){
+    return (n*(n + 1)/2);
+}
+
+int main(){
+    int n;
+
+
+    std::cout << "Dame un valor de n que sea positivo:\n";
+    std::cin >> n;
+
+    if (n>=0){
+        std::cout << "El resultado con suma iterativa es de: " << sumaIterativa(n) << "\n";
+
+        std::cout << "El resultado con suma Recursiva es de: " << sumaRecursiva(n) << "\n";
+
+        std::cout << "El resultado con suma Direvta es de: " << sumaDirecta(n) << "\n";
+    }
+    else{
+        std::cout << "El numero no es positivo\n";
+    }
+
+}
+
