fb3f3520f85ab06c69a01d24bef88207ad779d56
oznerx <A01742377@tec.mx>
2022-08-19 00:20:34 +0000

    Ordenamiento

--- BusquedaOrdenamiento.cpp
+++ BusquedaOrdenamiento.cpp
@@ -4,9 +4,11 @@
 //Nombre
 //Matricula
 
+//Se crea una función que retorna el otro vector con los mismos valores que el introducido pero ya esta ordenado con el método de Intercambio
+//Es de orden O(n^2)
 std::vector<int> ordenaIntercambio(std::vector<int> vec, int n){ 
-    int i, j, aux; 
-    for (i = 0 ; i <= n-2 ; i++){
+    int i, j, aux; /* se realizan n-1 pasadas, vec[o] ... vec[n-2] */
+    for (i = 0 ; i <= n-2 ; i++){ /* coloca mínimo de vec[i+1]...vec[n-1] en vec[i] */
         for (j = i+1 ; j <= n-1 ; j++){
             if (vec[i] > vec[j]){
                 aux = vec[i];
@@ -18,11 +20,12 @@ std::vector<int> ordenaIntercambio(std::vector<int> vec, int n){
     return vec;
 }
 
-
-
+//Se crea una función que retorna el otro vector con los mismos valores que el introducido pero ya esta ordenado con el método de Burbuja
+// En el mejor caso es de O(n)
+//En el peor caso es de O(n^2)
 std::vector<int> ordenaBurbuja(std::vector<int> vec, int n){
-    int tmp;
-    bool interruptor = true;
+    int tmp; //Variable temporal
+    bool interruptor = true; //Para saber cuando acabar el ciclo
     for (int i = 0;i < n-1 && interruptor;i++){
         interruptor = false;
         for (int j = 0; j<n-1-i;j++){
@@ -34,17 +37,19 @@ std::vector<int> ordenaBurbuja(std::vector<int> vec, int n){
             }
         }
     }
-    return vec;
+    return vec; //se retorna el nuevo vector ya ordenado
 }
 
-void ordenaMergeIntervalos(std::vector<int>& vector,int s, int m, int e){
-	std::vector<int> temp;
+
+//Creamos una funcion con la que manejamos los intervalos para depues acomodar los números 
+void ordenaMergeIntervalos(std::vector<int>& vector,int low, int mid, int high){
+	std::vector<int> temp; //Creamos un vector temporal 
 
 	int i, j;
-	i = s;
-	j = m + 1;
+	i = low;
+	j = mid + 1;
 
-	while (i <= m && j <= e) {
+	while (i <= mid && j <= high) {
 
 		if (vector[i] <= vector[j]) {
 			temp.push_back(vector[i]);
@@ -57,46 +62,48 @@ void ordenaMergeIntervalos(std::vector<int>& vector,int s, int m, int e){
 
 	}
 
-	while (i <= m) {
+	while (i <= mid) {
 		temp.push_back(vector[i]);
 		++i;
 	}
 
-	while (j <= e) {
+	while (j <= high) {
 		temp.push_back(vector[j]);
 		++j;
 	}
 
-	for (int i = s; i <= e; ++i)
-		vector[i] = temp[i - s];
+	for (int i = low; i <= high; ++i)
+		vector[i] = temp[i - low];
 }
 
-void ordenaMerge(std::vector<int>& vector, int s, int e) {
-	if (s < e) {
-		int m = (s + e) / 2;
-		ordenaMerge(vector, s, m);
-		ordenaMerge(vector, m + 1, e);
-		ordenaMergeIntervalos(vector, s, m, e);
+//Creamos una función para utilizar los limites 
+//La funcion es void porque utilizamor apuntadores 
+void ordenaMerge(std::vector<int>& vector, int low, int high) {
+	if (low < high) { // Verificamos que los limites son posibles 
+		int mid = (low + high) / 2; //obtenemos el valor de la mitad 
+		ordenaMerge(vector, low, mid); //llamamos la parte inferior
+		ordenaMerge(vector, mid + 1, high); //llamamos la parte superior
+		ordenaMergeIntervalos(vector, low, mid, high);
 	}
 }
 
-
+//Creamos una funcion de busqueda binaria para buscar por mitades 
 int busqBinaria(std::vector<int>& vec, int low, int high, int num){
 
-    while (low <= high) {
+    while (low <= high) { // Verificamos que los limites sean posibles 
 
-        int mid = low + (high - low) / 2;
+        int mid = low + (high - low) / 2; //obtenemos la mitad 
 
-        if (vec[mid] == num){
+        if (vec[mid] == num){ //si se encuentra el valor se retorna
             return mid;
         }
 
-        if (vec[mid] < num){
+        if (vec[mid] < num){ //si no se encuentra el valor se va a la mitad superior 
             low = mid + 1;
         }
 
         else{
-            high = mid - 1;
+            high = mid - 1; //si no se encuentra el valor se va a la mitad inferior
         }
         
     }
@@ -104,12 +111,13 @@ int busqBinaria(std::vector<int>& vec, int low, int high, int num){
     return -1;
 }
 
+//Creamos una función para buscar secuencialmente
 int busqSecuencial(std::vector<int>& vector, int num){
-    bool found = false;
-    int location;
-    for(location=0; location<vector.size(); location++)
+    bool found = false; //Variable para saber cuando ya encontramos a nuestro objetivo 
+    int i; //se crea una variable donde guardaremos la ubicación de lo que queremos encontrar 
+    for(i=0; i<vector.size(); i++)
     {
-        if(vector.at(location) == num)
+        if(vector.at(i) == num)
         {
             found = true;
             break;
@@ -117,7 +125,7 @@ int busqSecuencial(std::vector<int>& vector, int num){
     }
     if(found)
     {
-        return location;
+        return i; //Cuando se encuentra el objetivo se retorna la posición en la que se encuentra
     }
     else
     {
@@ -127,43 +135,44 @@ int busqSecuencial(std::vector<int>& vector, int num){
 
 int main()
 {
-    std::vector<int> vec;
+    std::vector<int> vec; //Declaració del vector a utilizar 
     int n, low , mid, high, valor;
     std::cout << "¿Cúantos datos quieres que tenga el vector?\n";
     std::cin >> n;
 
-    for (int i = 0;i < n; i++){
+    for (int i = 0;i < n; i++){ //Se recorre el vector para agragar valores 
         int num;
-        std::cout << "Dame un número para la posición " << i+1 << ":\n";
+        std::cout << "Dame un número para la posición " << i << ":\n";
         std::cin >> num;
         vec.push_back(num);  
     } 
 
     std::cout << "\n";
     std::cout << "Utilizando el método de intercambio el vector tiene los valores en el siguiente orden : \n";
-    for(auto i=0;i<ordenaIntercambio(vec,n).size();i++){
+    for(auto i=0;i<ordenaIntercambio(vec,n).size();i++){ //Se recorre el nuevo vector para ir mostrando valor por valor 
         std::cout << ordenaIntercambio(vec,n)[i] << "\n";
     }
 
     std::cout << "\n";
     std::cout << "Utilizando el método de burbuja el vector tiene los siguientes valores en el siguiente orden : \n";
-    for(auto i=0;i<ordenaBurbuja(vec,n).size();i++){
+    for(auto i=0;i<ordenaBurbuja(vec,n).size();i++){ //Se recorre el nuevo vector para ir mostrando valor por valor 
         std::cout << ordenaBurbuja(vec,n)[i] << "\n";
     }
 
     std::cout << "\n";
     std::cout << "Utilizando el método de merge el vector tiene los valores en el siguiente orden : \n";
-    ordenaMerge(vec,0,n-1);    
-    for(auto i=0;i<vec.size();i++){
+    ordenaMerge(vec,0,n-1);    //Se utiliza la función para ordenar el vector ya existente 
+    for(auto i=0;i<vec.size();i++){ //Se recorre el nuevo vector para ir mostrando valor por valor
         std::cout << vec[i] << "\n";
     }
 
+    std::cout << "\n";
     std::cout << "Dame un valor a buscar en el vector\n";
     std::cin >> valor;
     low = 0;
     high = vec.size();
+    std::cout << "\n";
     std::cout << "Utilizando la busqueda secuencial el numero se encuentra en la posición  " << busqSecuencial(vec,valor) << " del vector (iniciando en 0)\n";
     std::cout << "Utilizando la busqueda binaria el numero se encuentra en la posición  " << busqBinaria(vec,0,vec.size(),valor) << " del vector (iniciando en 0)\n";
-
 
 }
