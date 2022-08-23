1dad7726ba3020fa6e78b99f64d16e012437f7c8
oznerx <A01742377@tec.mx>
2022-08-23 21:33:50 +0000

    Organizetxt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
+
+
+void search(std::vector <int>& vec , int sDate, int eDate){
+    for (int sDate;sDate < eDate;sDate++){
+        std::cout << "This is my C++ gitpod template\n";
+
+    }
+}
 
 int main()
 {
-
+/*
+    Abra el archivo de entrada llamado "bitacora.txt Descargar bitacora.txt " lealo y almacene los datos en un vector  (Formato del archivo bitacora.pdf Descargar Formato del archivo bitacora.pdf ).
+    Ordene la información por fecha para la realización de las búsquedas.
+    Solicite al usuario las fechas de inicio y fin de búsqueda de información.
+    Despliegue los registros correspondientes a esas fechas.
+    Almacenar en un archivo el resultado del ordenamiento.
+*/
     std::cout << "This is my C++ gitpod template\n";
+
+
 }
