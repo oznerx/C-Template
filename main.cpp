113caf80d3584237cd31fce01f7fcb0a11101e8e
oznerx <A01742377@tec.mx>
2022-08-24 06:16:00 +0000

    otxt

--- main.cpp
+++ main.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream> 
 
+void loadFromFile(std::vector <std::string>& data){
+    std::string str;
+    std::ifstream file("bitacora.txt");
+
+
+    // Read the next line from File untill it reaches the end.
+
+    while (std::getline(file, str))
+    {
+        // Line contains string of length > 0 then save it in vector
+        if(str.size() > 0){
+            data.push_back(str);
+        }
+    }
+
+    //Close The File
+    file.close();
+}
+/*
+void split (std::vector <std::string> splitVector, std::string seperator){
+
+
+    for (int x=0;x<=9;x++)
+    {
+        splitVector[x]="";
+    }
+
+    int currIndex = 0, i = 0;  
+    int startIndex = 0, endIndex = 0;  
+
+    while (i <= splitVector.size())  
+    {  
+        if (splitVector[i] == seperator || i == splitVector.size())  
+        {  
+            endIndex = i;  
+            std::string subStr = "";  
+            subStr.append(str, startIndex, endIndex - startIndex);  
+            splitVector[currIndex] = subStr;  
+            currIndex += 1;  
+            startIndex = endIndex + 1;  
+        }  
+        i++;  
+    }  
+}
 
 void search(std::vector <int>& vec , int sDate, int eDate){
     for (int sDate;sDate < eDate;sDate++){
         std::cout << "This is my C++ gitpod template\n";
-
     }
 }
-
+*/
 int main()
 {
 /*
@@ -18,7 +63,15 @@ int main()
     Despliegue los registros correspondientes a esas fechas.
     Almacenar en un archivo el resultado del ordenamiento.
 */
+
+    std::vector <std::string> data;
+    loadFromFile(data);
+
     std::cout << "This is my C++ gitpod template\n";
+    
+    for(int i = 0; i < data.size();i++){
+        std::cout << data[i] << "\n";
+    }
 
 
 }
