6aafb6de78e664f47ac983e989e71d41643d0b6f
oznerx <A01742377@tec.mx>
2022-09-25 05:40:56 +0000

    dictionary

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
-#include "Perro.hpp"
 #include <iostream>
+#include <unordered_map>
 
+  
 int main()
 {
-    Perro c(4,5);
-    c.salta();
+    // Declaring umap to be of <string, int> type
+    // key will be of string type and mapped value will
+    // be of int type
+    std::unordered_map< int , std::string> listaSuper = {
+        {1, "huevos"}, {2, "leche"}, {3, "tortillas"}
+    };
+  
+    // inserting values by using [] operator
+    listaSuper[4] = "cereal";
+  
+    // Traversing an unordered map
+    for (auto x : listaSuper){
+        std::cout << x.first << " " << x.second << "\n" ;
+    }  
 
-    std::cout << "This is my C++ gitpod template\n";
+    int num;
+    std::cout << "hasta que numero quieres la lista?\n";
+    std::cin >> num;
+    for ( auto x : listaSuper){
+        if (x.first < num + 1){
+            std::cout << x.first << " " << x.second << "\n";
+        }
+    }
+    /*
+    for (int i = 0;i < listaSuper[3];i++){
+        std::cout << "-----------" <<listaSuper[num];
+    }
+    */
+
 }
