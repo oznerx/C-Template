b9c5bf0ccdb771c625886fa5f23eccdec3031665
oznerx <A01742377@tec.mx>
2022-09-25 02:26:31 +0000

    nodes

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ template <class T>
 class Node
 {
 public:
+
     T data;
-    Node<T>* next;
+    Node<T>* next; // Se crea el apuntador tipo T que hace referencia al siguiente nodo
     Node(T dataValue) {
-        data = dataValue;
-        next = nullptr;
+        data = dataValue; // Se le agrega el dato al nodo 
+        next = nullptr; // Se le asigna un valor al siguiente nodo, si existe pero no tiene nada
     }
 
 };
@@ -21,11 +22,12 @@ template <class T>
 class LinkedList {
 private:
 
-    Node<T>* head;
+    Node<T>* head; // Se crea la cabeza (el primer dato que tiene)
+
 public:
-    int size = 0;
+    int size = 0; // Para solicitar el size de la lista sin consumir tanta memoria 
     LinkedList() {
-        head = nullptr;
+        head = nullptr; // Se indica que es una lista vacia pero existe 
     }
     /*
     Añade  T value a la lista
@@ -33,16 +35,16 @@ public:
     */
     void add(T value) {
         size++;
-        Node<T>* new_node = new Node<T>(value);
-        if (head == nullptr) {
-            head = new_node;
+        Node<T>* new_node = new Node<T>(value); // Se crea un nuevo nodo con el valor indicado en la funcion 
+        if (head == nullptr) { // Si la lista no tierne ningun valor aun, entonces nuestro nodo va como primer dato
+            head = new_node; 
         }
-        else {
-            Node<T>* current = head;
-            while (current->next != nullptr) {
+        else { // Si la lista ya tiene elementos (nodos) dentro, entonces: 
+            Node<T>* current = head; // Se crea una variable que dice que nos encontramos en el primer elemento
+            while (current->next != nullptr) { // Recorre todos los elementos de la lista hasta encontrar un elemento 'next' vacio 
                 current = current->next;
             }
-            current->next = new_node;
+            current->next = new_node; // Una vez llegado al ultimo nodo de la lista, se agrega el nuevo nodo como siguiente 
 
         }
 
@@ -53,33 +55,35 @@ public:
      Remueve un elemento en la posición index
      @Param int index: ìndice del elemento a añadir
      */
-    void removeAt(int index)
+    void removeAt(int index) 
     {
-        if (index < 0) {
+        if (index < 0) { // si la posicionn que quieres eliminar es negativa, simplemente te regresa 
             return;
         }
-        Node<T>* last = head;
+        Node<T>* last = head; // se crea la variable last para poder recorrer la lista
         Node<T>* previous = nullptr;
-        if (head == nullptr) {
+        if (head == nullptr) { // si el primer elemnto no existe, entonces solo se regresa 
             return;
         }
-        else {
+        else { // si la lista si tiene elementos, entonces se hace lo siguiente: 
             int i = 0;
-            while (i < index && last->next != NULL) {
-                previous = last;
-                last = last->next;
+            // basicamente se recorre la lista para asi terminar justo en el elemento a eliminar 
+            while (i < index && last->next != NULL) { // mientras que el elemento 'next' de 'last' osea el el valor actual no este vacio, entonces se hace lo siguiente:  
+                previous = last; // basicamente aqui 'previous' y 'last' tienen los mismos valores (el ultimo elemento recorrido)
+                last = last->next; // el valor de 'last' se convierte en el elemento actual 
                 i++;
             }
-            if (i == 0) {
-                head = last->next;
-                free(last);
-                size--;
+            // recuerda que 'last->next' es ahora el elemnto actual 
+            if (i == 0) { // si el elemento 'next' de 'last' osea el el valor actual esta vacio (el elemnto a borrar es el primero en la lista) se hara lo siguiente:
+                head = last->next; // nuetro primer elemento sera el actual, osea el de la posicion 1 ahora es el de la 0 
+                free(last); // se eliminara el primer elemto que en este caso es 'last'  
+                size--; // se reduce el size de la lista
                 return;
             }
-            if (i == index) {
-                previous->next = last->next;
-                size--;
-                free(last);
+            if (i == index) { // si el elemento se encuentra despues de recorrer la lista, se hara lo siguiente: 
+                previous->next = last->next; // se le asigna al siguiente elemento den 'previous' (actual) que ahora si es el actual 
+                size--; // se reduce el size de la lista
+                free(last); // se elimina el elemento actual de la lista
             }
         }
     }
@@ -91,12 +95,12 @@ public:
     T get(int index)
     {
         if (index < 0) {
-            return NULL;
+            return 0;
         }
         Node<T>* last = head;
         Node<T>* previous = nullptr;
         if (head == nullptr) {
-            return NULL;
+            return 0;
         }
         else {
             int i = 0;
@@ -109,7 +113,7 @@ public:
                 return last->data;
             }
         }
-        return NULL;
+        return 0;
     }
 
     void printList()
@@ -130,5 +134,6 @@ int main()
     LinkedList <int>* lista = new LinkedList<int>();
     lista->add(1);
     lista->add(2);
-    lista->printList();
+    //lista->printList();
+    cout << lista->get(0);
 }
