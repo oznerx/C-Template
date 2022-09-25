96094a4bfc4ac5168927e2787b5a8cac85b55b23
oznerx <A01742377@tec.mx>
2022-09-25 06:42:39 +0000

    simple

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-using namespace std;
+#include <fstream>
 
 template <class T>
 class Node
@@ -9,10 +9,10 @@ class Node
 public:
 
     T data;
-    Node<T>* next; // Se crea el apuntador tipo T que hace referencia al siguiente nodo
+    Node<T>* next; 
     Node(T dataValue) {
-        data = dataValue; // Se le agrega el dato al nodo 
-        next = nullptr; // Se le asigna un valor al siguiente nodo, si existe pero no tiene nada
+        data = dataValue;  
+        next = nullptr; 
     }
 
 };
@@ -22,118 +22,131 @@ template <class T>
 class LinkedList {
 private:
 
-    Node<T>* head; // Se crea la cabeza (el primer dato que tiene)
+    Node<T>* head;
 
 public:
-    int size = 0; // Para solicitar el size de la lista sin consumir tanta memoria 
+    int size = 0; 
     LinkedList() {
-        head = nullptr; // Se indica que es una lista vacia pero existe 
+        head = nullptr; 
     }
     /*
-    Añade  T value a la lista
-    @Param T value: elemento añadir del tipo T T
+    Añade un elemento T a la estructura de datos
+    @Param T value: (T) elemento añadir del tipo T 
     */
-    void add(T value) {
+    void create(T value) {
         size++;
-        Node<T>* new_node = new Node<T>(value); // Se crea un nuevo nodo con el valor indicado en la funcion 
-        if (head == nullptr) { // Si la lista no tierne ningun valor aun, entonces nuestro nodo va como primer dato
+        Node<T>* new_node = new Node<T>(value); 
+        if (head == nullptr) { 
             head = new_node; 
         }
-        else { // Si la lista ya tiene elementos (nodos) dentro, entonces: 
-            Node<T>* current = head; // Se crea una variable que dice que nos encontramos en el primer elemento
-            while (current->next != nullptr) { // Recorre todos los elementos de la lista hasta encontrar un elemento 'next' vacio 
+        else { 
+            Node<T>* current = head; 
+            while (current->next != nullptr) { 
                 current = current->next;
             }
-            current->next = new_node; // Una vez llegado al ultimo nodo de la lista, se agrega el nuevo nodo como siguiente 
+            current->next = new_node;  
 
         }
 
 
     }
-
     /*
-     Remueve un elemento en la posición index
-     @Param int index: ìndice del elemento a añadir
-     */
-    void removeAt(int index) 
+    Busca un elemento T en la posicion indicada dentro de la estructura de datos  
+    @Param T value: (int) indice en el cual se encuentra el dato T  
+    */
+    void read(int index)
     {
-        if (index < 0) { // si la posicionn que quieres eliminar es negativa, simplemente te regresa 
-            return;
+        if (index < 0) { 
+            std::cout << "no existe un elemento con posicion negativa";
         }
-        Node<T>* last = head; // se crea la variable last para poder recorrer la lista
+        Node<T>* last = head; 
         Node<T>* previous = nullptr;
-        if (head == nullptr) { // si el primer elemnto no existe, entonces solo se regresa 
-            return;
-        }
-        else { // si la lista si tiene elementos, entonces se hace lo siguiente: 
-            int i = 0;
-            // basicamente se recorre la lista para asi terminar justo en el elemento a eliminar 
-            while (i < index && last->next != NULL) { // mientras que el elemento 'next' de 'last' osea el el valor actual no este vacio, entonces se hace lo siguiente:  
-                previous = last; // basicamente aqui 'previous' y 'last' tienen los mismos valores (el ultimo elemento recorrido)
-                last = last->next; // el valor de 'last' se convierte en el elemento actual 
-                i++;
-            }
-            // recuerda que 'last->next' es ahora el elemnto actual 
-            if (i == 0) { // si el elemento 'next' de 'last' osea el el valor actual esta vacio (el elemnto a borrar es el primero en la lista) se hara lo siguiente:
-                head = last->next; // nuetro primer elemento sera el actual, osea el de la posicion 1 ahora es el de la 0 
-                free(last); // se eliminara el primer elemto que en este caso es 'last'  
-                size--; // se reduce el size de la lista
-                return;
-            }
-            if (i == index) { // si el elemento se encuentra despues de recorrer la lista, se hara lo siguiente: 
-                previous->next = last->next; // se le asigna al siguiente elemento den 'previous' (actual) que ahora si es el actual 
-                size--; // se reduce el size de la lista
-                free(last); // se elimina el elemento actual de la lista
-            }
-        }
-    }
-
-    /*
-  Remueve un elemento en la posición index
-  @Param int index: ìndice del elemento a añadir
-  */
-    T get(int index)
-    {
-        if (index < 0) { // solo se verifica que el indice no sea negativo
-            return 0;
-        }
-        Node<T>* last = head; // se dice que nuestro ultimo nodo leido es igual al primero de la lista
-        Node<T>* previous = nullptr;
-        if (head == nullptr) { // si la lista no existe entonces solo regresa 
-            return 0;
+        if (head == nullptr) { 
+            std::cout << "la lista no existe";
         }
         else {
             int i = 0;
-            while (i < index && last->next != nullptr) { // mientras que el elemento 'next' de 'last' osea el el valor actual no este vacio, entonces se hace lo siguiente:  
-                previous = last; // basicamente aqui 'previous' y 'last' tienen los mismos valores (el ultimo elemento recorrido)
-                last = last->next; // el valor de 'last' se convierte en el elemento actual
+            while (i < index && last->next != nullptr) { 
+                previous = last; 
+                last = last->next;
                 i++;
             }
-            if (i == index) { // si se encuentra el indice del  valor despues del while, se hace lo siguiente:
-                return last->data; // como 'last' es el valor actual, se retorna la data del nodo
+            if (i == index) {
+                std::cout << last->data << "\n"; 
             }
         }
-        return 0;
+        std::cout << "no existe un elemento con esas caracteristicas" << "\n";
     }
+    /*
+    Actualiza un elemento T dentro de la estructura de datos en una posicion determinada 
+    @Param T value: (int) indice en el cual se desea modificar el dato T  
+    */
+    void update(){
 
-    void printList()
+    }
+    /*
+    Elimina un elemento dentro de la estructura de datos en una posicion indicada 
+    @Param T value: (int) indice en el cual se eliminara el dato T  
+    */
+    void del(int index) 
     {
-        Node<T>* current = head; // se dice que nos encontramos en el primer elemento de la lista  
-        while (current != NULL) { // si la lista si tiene elementos, se hara lo siguiente: 
-            cout << current->data << " "; // se imprime la data con un espacio entre cada valor 
-            current = current->next; // se asigna al valor siguiente de la lista con el proposito de recorrela
+        if (index < 0) {  
+            return;
         }
-        cout << "\n";
+        Node<T>* last = head; 
+        Node<T>* previous = nullptr;
+        if (head == nullptr) {  
+            return;
+        }
+        else { 
+            int i = 0;
+            while (i < index && last->next != NULL) {   
+                previous = last;
+                last = last->next; 
+                i++;
+            }
+            
+            if (i == 0) {
+                head = last->next; 
+                free(last);  
+                size--;
+                return;
+            }
+            if (i == index) {
+                previous->next = last->next; 
+                size--;
+                free(last); 
+            }
+        }
     }
 
+    void printAll()
+    {
+        Node<T>* current = head;  
+        while (current != NULL) {
+            std::cout << current->data << "\n"; 
+            current = current->next; 
+        }
+        std::cout << "\n";
+    }
 
 };
+/*
+Elimina un elemento dentro de la estructura de datos en una posicion indicada 
+@Param T value: (string) archivo a leer, (LinkedList <std::string>) estructura de datos   
+*/
+void readFile(std::string filePath,LinkedList <std::string>* lista ){
+    std::ifstream inputFile(filePath);
+    std::string line;
+    while (getline (inputFile , line)) {
+    lista->create(line);
+    }
+}
 
 int main()
 {
-    LinkedList <int>* lista = new LinkedList<int>();
-    lista->add(1);
-    lista->add(2);
-    //lista->printList();
-    cout << lista->get(0);
+    LinkedList <std::string>* lista = new LinkedList<std::string>();
+    readFile("bitacora.txt", lista);
+    //lista->printAll();
+     lista->read(16807);
 }
