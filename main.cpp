dc211072a828874bc06c3d23b579941afd7fee71
oznerx <A01742377@tec.mx>
2022-09-26 16:51:59 +0000

    add memory bugfixes

--- main.cpp
+++ main.cpp
@@ -75,14 +75,32 @@ public:
                 std::cout << last->data << "\n"; 
             }
         }
-        std::cout << "no existe un elemento con esas caracteristicas" << "\n";
+        //std::cout << "no existe un elemento con esas caracteristicas" << "\n";
     }
     /*
     Actualiza un elemento T dentro de la estructura de datos en una posicion determinada 
     @Param T value: (int) indice en el cual se desea modificar el dato T  
     */
-    void update(){
-
+    void update(int index, T newData){
+        if (index < 0) { // solo se verifica que el indice no sea negativo
+            std::cout << "No existe una posición negativa" ;
+        }
+        Node<T>* last = head; // se dice que nuestro ultimo nodo leido es igual al primero de la lista
+        Node<T>* previous = nullptr;
+        if (head == nullptr) { // si la lista no existe entonces solo regresa 
+            std::cout << "Error";
+        }
+        else {
+            int i = 0;
+            while (i < index && last->next != nullptr) { // mientras que el elemento 'next' de 'last' osea el el valor actual no este vacio, entonces se hace lo siguiente:  
+                previous = last; // basicamente aqui 'previous' y 'last' tienen los mismos valores (el ultimo elemento recorrido)
+                last = last->next; // el valor de 'last' se convierte en el elemento actual
+                i++;
+            }
+            if (i == index) { // si se encuentra el indice del  valor despues del while, se hace lo siguiente:
+                last->data = newData; // como 'last' es el valor actual, se retorna la data del nodo
+            }
+        }
     }
     /*
     Elimina un elemento dentro de la estructura de datos en una posicion indicada 
@@ -148,5 +166,16 @@ int main()
     LinkedList <std::string>* lista = new LinkedList<std::string>();
     readFile("bitacora.txt", lista);
     //lista->printAll();
-     lista->read(16807);
+    //lista->read(0);
+
+    std::string newData;
+    int index; 
+    std::cout << "Ingresa la posición del dato que deseas actualizar: ";
+    std::cin >> index;
+    std::cout << "Dato a actualizar: ";
+    lista->read(index);
+    std::cout << "Ingresa un nuevo dato:" << "\n";
+    std::cin >> newData;
+    std::cout << "Actualización exitosa" << "\n";
+    lista->read(index);
 }
