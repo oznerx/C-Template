95ae428dcccb4c66be109313cde74c988636ec88
oznerx <A01742377@tec.mx>
2022-10-10 16:49:02 +0000

    bst

--- main.cpp
+++ main.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
 
+/*
+Ozner Axel Leyva Mariscal
+A01742377
+16 de Octubre del 2022
+En este código se implementarán y agregarán las funcionalidades avanzadas 
+al ADT de un BST con las funcionalidades fundamentales
+*/
+
+/*
+Desplegará cada uno de los datos almacenados en el BST dependiendo del parámetro en entrada
+@Param:(int opc) opción del orden en el que se quiere desplegar los datos 
+Salida: nada
+Complejidad de tiempo: O()
+Complejidad de espacio: O()
+*/
+void visit (int opc){
+    //validar si es un BST válido
+}
+
+/*
+Regresará la altura del BST
+@Param: nada
+Salida: (int height) altura del BST
+Complejidad de tiempo: O()
+Complejidad de espacio: O()
+*/
+int height (){
+    //validar si es un BST válido
+    int height;
+    return height;
+}
+
+/*
+Desplegará los ancestros de un dato
+@Param: (int data) El dato del cual se desea conocer los ancestros
+Salida: nada
+Complejidad de tiempo: O()
+Complejidad de espacio: O()
+*/
+void ancestors (int data){
+    //validar si es un BST válido
+}
+
+/*
+Regresará un entero que indica el nivel en que se encuentra un dato, 
+o -1 en caso de que no se encuentre en el BST
+@Param: (int data) El dato a buscar su nivel
+Salida: (int level) Entero indicando el nivel del dato en el BST, o -1 si no se encuentra
+Complejidad de tiempo: O()
+Complejidad de espacio: O()
+*/
+int whatlevelamI (int data){
+    //validar si es un BST válido
+    int level; // retorna -1 si no se encuentra 
+    return level;
+}
+
+
 int main()
 {
-
-    std::cout << "This is my C++ gitpod template\n";
+    std::cout << "Árboles\n";
 }
