3789a50a8a4fa838c83cde38b62cd59d9fc89354
oznerx <A01742377@tec.mx>
2022-10-17 16:13:13 +0000

    temp

--- main.cpp
+++ main.cpp
@@ -2,87 +2,122 @@
 using namespace std;
 
 class Heap {
+
 public:
+
     int heapArray[1000];
-   int size = 0;
-   Heap() {
-   
-   }
-   void push(int value) {
-           heapArray[size] = value;
-           size++;
-           if (size > 1) {
-               swiftUp((size / 2) - 1);
-           }
-   }
-   void pop() {
-       
-       int temp = heapArray[size - 1];
-       heapArray[size-1] = heapArray[0];
-       heapArray[0] = temp;
-       size--;
-       if (size > 1) {
-           swiftDown(0);
-       }
-   }
+    int s = 0;
+    
+    Heap() {
 
-   void swap(int x, int y) {
-       int temp = heapArray[x];
-       heapArray[x] = heapArray[y];
-       heapArray[y] = temp;
+    }
 
-   }
+    void push(int value) {
+
+        heapArray[s] = value;
+        s++;
+
+        if (s > 1) {
+            swiftUp((s / 2) - 1);
+        }
+
+    }
+
+    void pop() {
+        
+        int temp = heapArray[s - 1];
+        heapArray[s-1] = heapArray[0];
+        heapArray[0] = temp;
+        s--;
+
+        if (s > 1) {
+            swiftDown(0);
+        }
+
+    }
+
+    void swap(int x, int y) {
+        
+        int temp = heapArray[x];
+        heapArray[x] = heapArray[y];
+        heapArray[y] = temp;
+
+    }
 
 
-   void  swiftUp(int rootIndex) {
-       int max = rootIndex;
-       int left = (2 * rootIndex) + 1;
-       int right = (2 * rootIndex) + 2;
-       if (left<size&&heapArray[max] < heapArray[left]) {
-           max = left;
-       }
-       if (right < size && heapArray[max] < heapArray[right]) {
-           max = right;
-       }
+    void  swiftUp(int rootIndex) {
+        
+        int max = rootIndex;
+        int left = (2 * rootIndex) + 1;
+        int right = (2 * rootIndex) + 2;
 
-       if (max != rootIndex) {
-       
-           swap(max, rootIndex);
-           swiftUp((rootIndex-1)/2);
-       }
-   
-   }
+        if (left<s&&heapArray[max] < heapArray[left]) {
+            max = left;
+        }
 
-   void  swiftDown(int rootIndex) {
-       int max = rootIndex;
-       int left = (2 * rootIndex) + 1;
-       int right = (2 * rootIndex) + 2;
-       if (left < size && heapArray[max] < heapArray[left]) {
-           max = left;
-       }
-       if (right < size && heapArray[max] < heapArray[right]) {
-           max = right;
-       }
+        if (right < s && heapArray[max] < heapArray[right]) {
+            max = right;
+        }
 
-       if (max != rootIndex) {
+        if (max != rootIndex) {
+        
+            swap(max, rootIndex);
+            swiftUp((rootIndex-1)/2);
+        }
 
-           swap(max, rootIndex);
-           swiftDown(max);
-       }
+    }
 
-   }
-   void print() {
-       for (int i = 0; i < size; i++) {
-           cout << heapArray[i] << " ";
-       }
-       cout << endl;
-   }
-   void print(int fullSize) {
-       for (int i = 0; i < fullSize; i++) {
-           cout << heapArray[i] << " ";
-       }
-       cout << endl;
-   }
+    void  swiftDown(int rootIndex) {
+
+        int max = rootIndex;
+        int left = (2 * rootIndex) + 1;
+        int right = (2 * rootIndex) + 2;
+
+        if (left < s && heapArray[max] < heapArray[left]) {
+            max = left;
+        }
+
+        if (right < s && heapArray[max] < heapArray[right]) {
+            max = right;
+        }
+
+        if (max != rootIndex) {
+            swap(max, rootIndex);
+            swiftDown(max);
+        }
+
+    }
+
+    void print() {
+
+        for (int i = 0; i < s; i++) {
+            cout << heapArray[i] << " ";
+        }
+
+        cout << endl;
+
+    }
+
+    void print(int fullSize) {
+
+        for (int i = 0; i < fullSize; i++) {
+            cout << heapArray[i] << " ";
+        }
+
+        cout << "\n";
+    }
+
+    void top() {
+
+    }
+
+    void empty() {
+
+    }
+
+    void size(){
+
+    }
 
 };
 
