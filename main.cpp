e2bf7fec3a370b23de79c83d2063eaddb83cf596
oznerx <A01742377@tec.mx>
2022-11-06 09:14:35 +0000

    ahi va

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <unordered_map>
+#include <map>
+#include <list>
 
 class Graph {
 private:
 
     bool** adjMatrix;
+    std::map<int, std::list<int> > adjList;
     int numVertices;
     int numEdges;
-    std::vector<std::vector<int>> adjList;
+
+    std::map<int, bool> visited;
+    
 
 public:
 
@@ -42,15 +47,16 @@ public:
 
     void loadList(int n, int m ) {
 
-        // Set the max num of edges
+        this->numVertices = n;
         this->numEdges = m;
 
-        for(int i = 0; i < n; i++) {
-            std::vector<int> node;
-            adjList.push_back(node);
+        for (int i = 0; i < numVertices; i++) {
+            std::list<int> temp;
+            adjList.emplace(i, temp);
         }
 
-        
+        //std::cout << adjList.size() << "\n";
+
     } 
 
     void addEdgeToMatrix(int i, int j) {
@@ -64,9 +70,7 @@ public:
     }
 
     void addEdgeToList(int a, int b) {
-        if (a < numVertices && b < numVertices) {
-            adjList[a].push_back(b);
-        }
+        adjList[a].push_back(b); // Add b to a’s list.
     }
 
     void removeEdgeFromList(int i, int j) {
@@ -87,11 +91,64 @@ public:
     }
 
     void printList() {
-        for (int i = 0; i < numVertices ;i++) {
-            for(int j = 0; j < adjList[i].size() ;j++) {
-                std::cout << adjList[i][j];
+
+        std::map<int, std::list<int>>::iterator i;
+        for (i=adjList.begin(); i!=adjList.end(); i++) {
+
+            std::cout << i->first << " : ";
+
+            for (auto j : i->second) {
+                std::cout << j << " ";
             }
+
+            std::cout << "\n";
+
         }
+
+    }
+
+    void DFSUtil(int a)
+    {
+
+        // Mark the current node as visited and print it
+        visited[a] = true;
+        std::cout << a << " ";
+    
+        // Recur for all the vertices adjacent to this vertex
+        std::list<int>::iterator i;
+        for (i = adjList[a].begin(); i != adjList[a].end(); ++i) {
+
+            if (!visited[*i]) {
+                DFSUtil(*i);
+            }
+                
+        }
+
+    }
+
+    /*Imprima el Recorrido de DFS (Profundidad) a partir de nodo inicial.*/
+    void DFS(int a, int b) {
+
+        /*Hacer DFS con la matriz*/
+        
+        // Call the recursive helper function to print DFS
+        // traversal starting from all vertices one by one
+        for (auto i : adjList) {
+
+            if (visited[i.first] == false) {
+                DFSUtil(i.first);
+            }
+
+        }
+
+        std::cout << "\n";
+ 
+                
+    }
+
+    /*Imprima el Recorrido de BFS (Anchura) a partir de nodo inicial.o.*/
+    void BFS(int a, int b) {
+        /*Hacer BFS con la lista*/
     }
 
 
@@ -117,10 +174,14 @@ int main()
 
 
     Graph *g2 = new Graph();
-    g2->loadList(6, 3);
-    g2->addEdgeToList(1, 3);
+    g2->loadList(6, 3); // 6 nodos/vertices y 3 aristas 
+
+    g2->addEdgeToList(0, 3);
     g2->addEdgeToList(5, 2);
     g2->addEdgeToList(3, 4);
+    g2->addEdgeToList(3, 5);
+    
     g2->printList();
+    g2->DFS(0,6);
 
 }
