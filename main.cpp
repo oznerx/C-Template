0ce6cb5b32998e041de512bd639f58bd95d9660c
oznerx <A01742377@tec.mx>
2022-11-10 18:23:13 +0000

    working

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <vector>
 #include <unordered_map>
 #include <map>
-#include <list>
 #include <stack>
+#include <queue>
+
 
 
 /*
@@ -21,7 +22,7 @@ private:
     int numEdges;
     int currentEdges;
     std::map<int, bool> visited;
-    std::map<int, std::list<int>> adjList;
+    std::map<int, std::vector<int>> adjList;
     std::stack<int> stack; 
 
     /*
@@ -34,7 +35,7 @@ private:
     void loadList() {
 
         for (int i = 0; i < numVertex; i++) {
-            std::list<int> temp;
+            std::vector<int> temp;
             adjList.emplace(i, temp);
         }
 
@@ -160,10 +161,79 @@ public:
     }
 
     /*bipartiteGraph*/
-    bool bipartiteGraph() {
+    bool _bipartiteGraph(int colorArr[]) {
+
+        colorArr[0] = 1;
+    
+        // Create a queue (FIFO) of vertex numbers a
+        // nd enqueue source vertex for BFS traversal
+        std::queue<int> q;
+        q.push(0);
+    
+        // Run while there are vertices in queue (Similar to
+        // BFS)
+        while (!q.empty()) {
+            // Dequeue a vertex from queue ( Refer
+            // http://goo.gl/35oz8 )
+            int u = q.front();
+            q.pop();
+    
+            // Return false if there is a self-loop
+            if (adjList[u][u] == 1) {
+                return false;
+            }
+    
+            // Find all non-colored adjacent vertices
+            for (int v = 0; v < numVertex; ++v) {
+                // An edge from u to v exists and
+                // destination v is not colored
+                if (adjList[u][v] && colorArr[v] == -1) {
+                    // Assign alternate color to this
+                    // adjacent v of u
+                    colorArr[v] = 1 - colorArr[u];
+                    q.push(v);
+                }
+    
+                // An edge from u to v exists and destination
+                // v is colored with same color as u
+                else if (adjList[u][v] && colorArr[v] == colorArr[u]) {
+                    return false;
+                }
+            }
+        }
+    
+        // If we reach here, then all adjacent vertices can
+        // be colored with alternate color
         return true;
     }
     
+    bool bipartiteGraph() { 
+        // Create a color array to store colors assigned to all
+        // vertices. Vertex/ number is used as index in this
+        // array. The value '-1' of colorArr[i] is used to
+        // indicate that no color is assigned to vertex 'i'.
+        // The value 1 is used to indicate first color is
+        // assigned and value 0 indicates second color is
+        // assigned.
+        int colorArr[numVertex];
+        for (int i = 0; i < numVertex; ++i) {
+            colorArr[i] = -1;
+        }
+    
+        // This code is to handle disconnected graph
+        for (int i = 0; i < numVertex; i++) {
+            if (colorArr[i] == -1) {
+                if (_bipartiteGraph(colorArr) == false) {
+                    return false;
+                }
+            }
+        }
+
+    
+        return true;
+    }
+
+
     /*Diga si el Grafo Dirigido (DAG) es un árbol o no*/
     void isTree() {
 
@@ -205,13 +275,21 @@ int main()
     std::cout << "Recorrido por DFS: ";
     g->DFS(0);
     g->topologicalSort();
-*/
+*//*
     std::cout << "\n=================== Topological Sort ===================\n";
     Graph *g = new Graph();
     std::vector <std::pair<int, int>> edges = {{5,2}, {5,0}, {4,0}, {4,1}, {2,3}, {3,1}}; 
     g->loadGraph(6, 6, edges);
     g->topologicalSort();
-    std::cout << "\n";
+    std::cout << "\n";*/
+
+    std::cout << "\n=================== Is Bipartite ===================\n";
+    Graph *g = new Graph();
+    std::vector <std::pair<int, int>> edges = {{0,1}, {0,3}, {1,0}, {1,3}, {2,1}, {2,3}, {3,0}, {3,2}}; 
+    g->loadGraph(4, 8, edges);
+    std::cout << g->bipartiteGraph() << "\n";
+    g->print();
+    g->bipartiteGraph() ? std::cout << "Yes" : std::cout << "No";
 
 }
 
