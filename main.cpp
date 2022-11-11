43ef82468359a1da2becbf78a1711ed137360ced
oznerx <A01742377@tec.mx>
2022-11-11 22:23:03 +0000

    hamilton

--- main.cpp
+++ main.cpp
@@ -1,362 +1,99 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <unordered_map>
-#include <map>
-#include <list>
+using namespace std;
 
-/*
-Ozner Axel Leyva Mariscal
-A01742377
-9 de Noviembre del 2022
-En este código se implementará un grafo direccionado asi como algunas de sus funcionalidades como el recorrido por DFS y BFS. 
-*/
+struct Arista {
+    int origen, destino;
+};
 
-class Graph {
-private:
-
-    bool** adjMatrix;
-    std::map<int, std::list<int> > adjList;
-    int numVertex;
-    int numEdges;
-    int currentEdges;
-    std::map<int, bool> visited;
-    int edgesCounter;
-
-    /*
-    Carga una matriz de adjacencia
-    @Param: nada
-    Salida: nada
-    Complejidad de tiempo: O(V^2)
-    Complejidad de espacio: O(V^2) 
-    */
-    void loadMatrix() {
-
-        adjMatrix = new bool*[numVertex];
-        
-        for (int i = 0; i < numVertex; i++) {
-            adjMatrix[i] = new bool[numVertex];
-
-            for (int j = 0; j < numVertex; j++) {
-                adjMatrix[i][j] = false;
-            }
-
-        }
-
-    }
-
-    /*
-    Carga una lista de adjacencia
-    @Param: nada
-    Salida: nada
-    Complejidad de tiempo: O(V)
-    Complejidad de espacio: O(V) 
-    */
-    void loadList() {
-
-        for (int i = 0; i < numVertex; i++) {
-            std::list<int> temp;
-            adjList.emplace(i, temp);
-        }
-
-    } 
-
-    /*
-    Agrega un arco entre 2 vértices de una matriz de adjacencia 
-    @Param: (int a) vértice al cual se le va a agregar la conexión , (int b) vértice el cual será conectado
-    Salida: nada
-    Complejidad de tiempo: O(1)
-    Complejidad de espacio: O(1) 
-    */
-    void addEdgeToMatrix(int a, int b) {
-        adjMatrix[a][b] = true;
-    }
-
-    /*
-    Agrega un arco entre 2 vértices de una lista de adjacencia
-    @Param: (int a) vértice al cual se le va a agregar la conexión , (int b) vértice el cual será conectado
-    Salida: nada
-    Complejidad de tiempo: O(1)
-    Complejidad de espacio: O(1) 
-    */
-    void addEdgeToList(int a, int b) {
-        adjList[a].push_back(b); 
-    }
-
-    /*
-    Imprime una idea general de la matriz de adjacencia del grafo creado
-    @Param: nada
-    Salida: nada
-    Complejidad de tiempo: O(V^2)
-    Complejidad de espacio: O(1) 
-    */
-    void printMatrix() {
-            
-        for (int i = 0; i < numVertex; i++) {
-
-            std::cout << i << " : ";
-
-            for (int j = 0; j < numVertex; j++) {
-                std::cout << adjMatrix[i][j] << " " ;
-            }
-
-            std::cout << "\n";
-
-        }
-
-    }
-
-    /*
-    Imprime una idea general de la lista de adjacencia del grafo creado
-    @Param: nada
-    Salida: nada
-    Complejidad de tiempo: O(V + E)
-    Complejidad de espacio: O(1) 
-    */
-    void printList() {
-
-        for (auto i : adjList) {
-
-            std::cout << i.first << " : ";
-
-            for (auto j : i.second) {
-                std::cout << j << " ";
-            }
-
-            std::cout << "\n";
-
-        }
-
-    }
-
-    /*
-    Establece todos los nodos como a "no visitados"
-    @Param: nada
-    Salida: nada
-    Complejidad de tiempo: O(V)
-    Complejidad de espacio: O(1) 
-    */
-    void clearVisited() {
-
-        for (int i = 0; i < visited.size() ;i++) {
-            visited[i] = false;
-        }
-
-    }
-    
+class Grafo
+{
 public:
-
-    /* Constructor de la clase de grafo */
-    Graph() {
-        int numVertex = 0;
-        int numEdges = 0;
-        int currentEdges = 0;
-    }
+    vector<vector<int>> adyacencias;
 
     /*
-    Carga los arcos del grafo, los almacena en una Matriz de Adjacencia y en una Lista de Adjacencia
-    @Param:(int n) Cantidad de Vertices, (int m) Cantidad de Arcos, (std::vector <std::pair<int, int>> edges) lista de los arcos entre aristas
-    Salida: nada
-    Complejidad de tiempo: O(V^2)
-    Complejidad de espacio: O(V^2) 
+     Construye un grafo de n vertices recibiendo un vector de aristas
+     @param vector<Arista> const& aristas: vecotor con aristas
+     @param int n: cantidad de nodos
     */
-    void loadGraph(int n, int m, std::vector <std::pair<int, int>> edges) {
-
-        this->numVertex = n;
-        this->numEdges = m;
-        loadList();
-        loadMatrix();
-
-        for (int i = 0; i < edges.size() ; i++) {
-            addEdge(edges[i].first, edges[i].second);
+    Grafo(vector<Arista> const& aristas, int n)
+    {
+        adyacencias.resize(n);
+        for (Arista arista : aristas)
+        {
+            int origen = arista.origen;
+            int destino = arista.destino;
+            adyacencias[origen].push_back(destino);
+            adyacencias[destino].push_back(origen);
         }
-
-    }
-
-    /*
-    Agrega un arco entre 2 vertices
-    @Param: (int a) vertice al cual se le va a agregar la conexión , (int b) vertice el cual será conectado
-    Salida: nada
-    Complejidad de tiempo: O(1)
-    Complejidad de espacio: O(1) 
-    */
-    void addEdge(int a, int b) {
-
-        if (currentEdges < numEdges) {
-            currentEdges++;
-            addEdgeToMatrix(a, b);
-            addEdgeToList(a, b);
-        }
-
-        else {
-            std::cout << "Numero de conexiones máximo alcanzado\n";
-            return;
-        }
-        
-    }
-
-    /*
-    Imprime una idea general de la estrcutura del grafo creado
-    @Param: nada
-    Salida: nada
-    Complejidad de tiempo: O(V^2)
-    Complejidad de espacio: O(1) 
-    */
-    void print() {
-
-        std::cout << "\nLa matriz se creó con la siguiente estructura:\n";
-        printMatrix();
-        std::cout << "\nLa lista se creó con la siguiente estructura:\n";
-        printList();
-        std::cout << "\n";
-
-    }
-
-    /*
-    Imprime el Recorrido de DFS de una lista de adjacencia a partir de un nodo inicial
-    @Param: (int a) nodo inicial
-    Salida: nada
-    Complejidad de tiempo: O(V + E)
-    Complejidad de espacio: O(V) 
-    */
-    void listDFS(int a) {
-
-        visited[a] = true;
-        std::cout << a << " ";
-    
-        std::list<int>::iterator i;
-        for (i = adjList[a].begin(); i != adjList[a].end(); ++i) {
-
-            if (!visited[*i]) {
-                listDFS(*i);
-            }
-                
-        }
-            
-    }
-
-    /*
-    Imprime el Recorrido de DFS de una matriz de adjacencia a partir de un nodo inicial
-    @Param: (int a) nodo inicial
-    Salida: nada
-    Complejidad de tiempo: O(V^2)
-    Complejidad de espacio: O(V) 
-    */
-    void _DFS(int a) {
-
-        visited[a] = true;
-        std::cout << a << " ";
-
-        for (int i = 0; i < sizeof(adjMatrix[a]); ++i) {
-
-            if (adjMatrix[a][i] == true && (!visited[i])) {
-                _DFS(i);
-            }
-
-        }
-           
-    }
-
-    /*
-    Ayudante de del método _DFS
-    @Param: (int a) nodo inicial
-    Salida: nada
-    Complejidad de tiempo: O(V^2)
-    Complejidad de espacio: O(V) 
-    */
-    void DFS(int a) {
-        clearVisited();
-        _DFS(a);
-        std::cout << "\n";
-    }
-
-    /*
-    Imprime el Recorrido de BFS de una lista de adjacencia a partir de un nodo inicial
-    @Param: (int a) nodo inicial
-    Salida: nada
-    Complejidad de tiempo: O(V + E)
-    Complejidad de espacio: O(V) 
-    */
-    void BFS(int a) {
-
-        clearVisited();
-        std::list<int> queue;
-        visited[a] = true;
-        queue.push_back(a);
-    
-        while (!queue.empty()) {
-
-            a = queue.front();
-            std::cout << a << " ";
-            queue.pop_front();
-
-            for (auto i: adjList[a]) {
-
-                if (!visited[i]) {
-                    visited[i] = true;
-                    queue.push_back(i);
-                }
-
-            }
-
-        }
-
-        std::cout << "\n";
-
     }
 
 };
 
-int main()
+/*
+* Imprime ruta
+*/
+void imprimirRuta(vector<int> const& ruta)
 {
+    for (int i : ruta) {
+        cout << i << ' ';
+    }
+    cout << endl;
+}
 
-    std::cout << "\n=================== Primer Grafo ===================\n";
-    Graph *g = new Graph();
-    std::vector <std::pair<int, int>> edges1 = {{0,1}, {1,2}, {1,3}, {2,0}, {2,5}, {3,4}, {4,5}}; 
-    g->loadGraph(6, 8, edges1);
-    g->print();
-    std::cout << "Recorrido por DFS: ";
-    g->DFS(0);
-    std::cout << "\nRecorrido por BFS: ";
-    g->BFS(0);
+void caminosHamiltonianos(Grafo const& grafo, int nodoActual, vector<bool>& visitados, vector<int>& ruta, int n)
+{
+    if (ruta.size() == n) {
 
-    std::cout << "\n=================== Segundo Grafo ===================\n";
-    Graph *g2 = new Graph();
-    std::vector <std::pair<int, int>> edges2 = {{0,1}, {0,2}, {1,3}, {2,3}, {2,4}, {3,5}, {5,6}}; 
-    g2->loadGraph(7, 7, edges2);
-    g2->print();
-    std::cout << "Recorrido por DFS: ";
-    g2->DFS(0);
-    std::cout << "\nRecorrido por BFS: ";
-    g2->BFS(0);
+        imprimirRuta(ruta);
+        
+        for (int j = 0 ; j < grafo.adyacencias[nodoActual].size(); j++ ) {
+            
+            if ( grafo.adyacencias[nodoActual][j] == ruta[0]) {
+                cout << "Es un ciclo\n";
+            }
 
-    std::cout << "\n=================== Tercer Grafo ===================\n";
-    Graph *g3 = new Graph();
-    std::vector <std::pair<int, int>> edges3 = {{0,1}, {0,2}, {1,2}, {1,3}, {1,4}, {2,5}, {2,6}, {4,3}, {5,6}}; 
-    g3->loadGraph(7, 9, edges3);
-    g3->print();
-    std::cout << "Recorrido por DFS: ";
-    g3->DFS(0);
-    std::cout << "\nRecorrido por BFS: ";
-    g3->BFS(0);
+        }
 
-    std::cout << "\n=================== Cuarto Grafo ===================\n";
-    Graph *g4 = new Graph();
-    std::vector <std::pair<int, int>> edges4 = {{0,1}, {0,3}, {3,4}, {4,5}, {4,2}}; 
-    g4->loadGraph(6, 5, edges4);
-    g4->print();
-    std::cout << "Recorrido por DFS: ";
-    g4->DFS(0);
-    std::cout << "\nRecorrido por BFS: ";
-    g4->BFS(0);
+        return;
+    }
+
+    for (auto i : grafo.adyacencias[nodoActual]) {
+        visitados[i] = true;
+        ruta.push_back(i);
+        caminosHamiltonianos(grafo, i, visitados, ruta, n);
+        visitados[i] = false;
+        ruta.pop_back();
+    }
 
 }
 
-/*
-Referencias: 
+void GeneraCaminosHamiltonianos(Grafo const& grafo, int n)
+{
+    vector <bool> visitados(n,false);
 
-    GeeksforGeeks. (2022). Breadth First Search or BFS for a Graph. https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph.
+    vector <int> ruta;
 
-    GeeksforGeeks. (2022). Depth First Search or DFS for a Graph. https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph.
+    for (auto i = 0; i < n ;i++) {
 
-*/
+        visitados[i] = true;
+        ruta.push_back(i);
+        caminosHamiltonianos(grafo, i,  visitados, ruta, n);
+        visitados[i] = false;
+        ruta.pop_back();
+
+    }
+
+
+}
+
+int main()
+{
+    int n = 5;
+    vector<Arista> aristas = {
+        {0, 1}, {0, 2}, {0, 3}, {1, 2}, {1, 3}, {2, 3},{4,3}, {4,0}
+    };
+    Grafo grafo(aristas, n);
+    GeneraCaminosHamiltonianos(grafo, n);
+    return 0;
+}
