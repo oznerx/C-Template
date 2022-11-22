0279946133d1ec397593783d0ead3216693f623c
oznerx <A01742377@tec.mx>
2022-11-22 07:09:55 +0000

    done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <stack>
 #include <queue>
+#include <unordered_map>
 
 /*
 Ozner Axel Leyva Mariscal
@@ -18,10 +19,11 @@ private:
     int numVertex;
     int numEdges;
     int currentEdges;
-    std::map<int, bool> visited;
+    std::vector<bool> visited;
     std::vector<std::vector<int>> adjList;
     std::stack<int> stack; 
     std::vector<int> fanOut; // usar mapa si se trata de nodos descritos con letras 
+    std::unordered_map<int, bool> conection; // 
 
     /*
     Carga una lista de adjacencia
@@ -33,6 +35,7 @@ private:
     void loadList() {
         adjList.resize(numVertex);
         fanOut.resize(numVertex, 0);
+        visited.resize(numVertex);
     } 
 
     /*
@@ -489,14 +492,118 @@ public:
 
     std::cout << "The farthest node from the node " << startNode << " is the node: " << farthest << "\n";
 
-}	
+    }	
+
+    void bfsSearch(int startNode, int nodeToSearch) {
+
+        clearVisited();
+        std::vector<int> time(numVertex, 0);
+
+        // Initialize with start node
+        time[startNode];
+        visited[startNode] = true;
+
+        // BFS search 
+        std::queue <int> q;
+        q.push(startNode);
+
+        while(!q.empty()) {
+
+            int currentNode = q.front();
+            q.pop();
+
+            for(int neighbor : adjList[currentNode]) {
+                // If we haven't enqueued the neighbor
+                if(!visited[neighbor]) {
+                    // Get its level and store it
+                    int neighborTime = time[currentNode] + 1;
+                    time[neighbor] = neighborTime;
+
+                    // Otherwise we add it to the queue and mark it as visited
+                    q.push(neighbor);
+                    visited[neighbor] = true;
+
+                }
+
+                if (neighbor == nodeToSearch) {
+                    std::cout << "El nodo " << nodeToSearch << " a partir del nodo " << startNode << ", se encontró en el tiempo " << time[nodeToSearch] << "\n"; 
+                    break;
+                }
+
+            }
+        }
+
+    }
+
+    /* Recorrido en grafo desconectado */
+    void bfsSearchDisconnected(int startNode, int color) {
+
+        std::vector<int> conjunto;
+        visited[startNode] = true;
+
+        // BFS search 
+        std::queue <int> q;
+        q.push(startNode);
+        conjunto.push_back(startNode);
+
+        while(!q.empty()) {
+
+            int currentNode = q.front();
+            q.pop();
+
+            for(int neighbor : adjList[currentNode]) {
+
+                if(!visited[neighbor]) {
+                    q.push(neighbor);
+                    visited[neighbor] = true;
+                    conjunto.push_back(neighbor);
+                }
+
+            }
+        }
+
+        std::cout << "El conjunto " << color << " contiene los siguientes nodos: \n";
+        for (auto i : conjunto) {
+            std::cout << i << " ";
+        }
+        std::cout << "\n";
+
+    }  
+
+    void printVisited() {
+        for (int j = 0; j < visited.size() ; j++) {
+            std::cout << visited[j] << "\n";
+        }
+    }
+
+    void disconnectedSearch(int startNode, int conjuntos) {
+
+        clearVisited();
+
+        int i = 1;
+        while (i < conjuntos) {
+
+            if (i == 1) {
+                bfsSearchDisconnected(startNode, i);
+            }
+
+            i++;
+            for (int j = 0; j < visited.size() ; j++) {
+                if (!(visited[j])) {
+                    bfsSearchDisconnected(j, i);
+                }
+            }
+
+        }
+        
+    }
 
 };
 
 int main()
 {
 
-    std::cout << "\n======================== Primer Grafo ========================\n";
+    /*std::cout << "\n======================== Primer Grafo ========================\n";
     Graph *g1 = new Graph();
     std::vector <std::pair<int, int>> edges1 = {{5,2}, {5,0}, {4,0}, {4,1}, {2,3}, {3,1}}; 
     g1->loadGraph(6, 6, edges1);
@@ -505,7 +612,14 @@ int main()
     g1->getMaxFanOut();
     std::cout << "\n";
     g1->farthestNode(5);
+    g1->bfsSearch(5, 1);*/
     
+    Graph *g2 = new Graph();
+    std::vector <std::pair<int, int>> edges2 = {{0,1}, {1,2}, {2,3}, {4,5}}; 
+    g2->loadGraph(6, 4, edges2); 
+    g2->print();
+    g2->disconnectedSearch(0, 2);
+
 }
 
 /*
